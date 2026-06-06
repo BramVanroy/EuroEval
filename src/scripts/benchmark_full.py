36031ba2090c0a7213f258a7a107f4af9d906172
Bram Vanroy <2779410+BramVanroy@users.noreply.github.com>
2026-06-06 10:17:57 +0200

    add force flag

--- src/scripts/benchmark_full.py
+++ src/scripts/benchmark_full.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-
+import json
 
 from euroeval import Benchmarker
 
@@ -9,10 +9,52 @@ def main(
     dataset: str,
     output_file: str = "benchmark_results.jsonl",
     cache_dir: str | None = None,
+    force: bool = False,
 ):
     pfout = Path(output_file)
     pfcache = pfout.parent / "cache" if cache_dir is None else Path(cache_dir)
     pfcache.mkdir(parents=True, exist_ok=True)
+
+    result_exists = False
+    if pfout.exists():
+        keep_data = []
+        for line in pfout.read_text(encoding="utf-8").splitlines():
+            if not line.strip():
+                continue
+            result = json.loads(line)
+            try:
+                res_model = result["model_info"]["id"]
+                res_dataset = result["evaluation_results"][0]["source_data"]["dataset_name"]
+            except KeyError:
+                continue
+            else:
+                if res_model == model and res_dataset == dataset:
+                    result_exists = True
+                    # Remove/overwrite existing results for the same model and dataset
+                    if force:
+                        continue
+                    else:
+                        keep_data.append(result)
+                else:
+                    keep_data.append(result)
+        if keep_data:
+            with pfout.open("w", encoding="utf-8") as fhout:
+                for result in keep_data:
+                    fhout.write(json.dumps(result) + "\n")
+
+    if result_exists and not force:
+        print(f"Results for model '{model}' on dataset '{dataset}' already exist in '{pfout}'. Use --force to overwrite.")
+        return
+
+    benchmarker = Benchmarker(cache_dir=str(pfcache), verbose=True)
+    for result in benchmarker.benchmark(
+        model=model,
+        dataset=dataset,
+        save_results=False,
+    ):
+        result.append_to_results(pfout)
+
+
 
     benchmarker = Benchmarker(cache_dir=str(pfcache), verbose=True)
     for result in benchmarker.benchmark(
@@ -30,10 +72,12 @@ if __name__ == "__main__":
     parser.add_argument("--dataset", type=str, required=True, help="The dataset to benchmark on, e.g., 'mmlu-nl-full'.")
     parser.add_argument("-o", "--output-file", type=str, default="benchmark_results.jsonl", help="The file to save the benchmark results to.")
     parser.add_argument("--cache-dir", type=str, help="The directory to cache the benchmark results in.")
+    parser.add_argument("--force", action="store_true", help="Whether to overwrite existing results for the same model and dataset.")
     args = parser.parse_args()
     main(
         model=args.model,
         dataset=args.dataset,
         output_file=args.output_file,
         cache_dir=args.cache_dir,
+        force=args.force
     )
